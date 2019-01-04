dd0a7f84462b225c596b5a6746caf2c63bedaa66
kmooog <kmooog@fout.jp>
2019-01-04 18:26:26 +0900

    Use mobX

--- 6-mobx-react/00_start_point/react-mobx/src/js/TodoList.js
+++ 6-mobx-react/00_start_point/react-mobx/src/js/TodoList.js
@@ -1,7 +1,42 @@
-import React from "react";
+ import React from "react";
+ import { observer } from "mobx-react";
 
-export default class TodoList extends React.Component {
-  render() {
-    return <h1>MobX</h1>;
-  };
-}
+ @observer
+ export default class TodoList extends React.Component {
+  createNew(e) {
+    if (e.which === 13) { // enter
+      this.props.store.createTodo(e.target.value);
+      e.target.value = "";
+    }
+  }
+
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+
+  toggleComplete(todo) {
+    todo.complete = !todo.complete;
+  }
+
+  clear(todo) {
+      delete this.props.store.todos.todo;
+  }
+
+   render() {
+    const { clearComplete, filter, filteredTodos, todos } = this.props.store;
+
+    const todoList = filteredTodos.map(todo => (
+       <li key={todo.id}>
+              <input type="checkbox" onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete} />
+              {todo.value}
+       </li>
+      ));
+    return <div>
+      <h1>todos</h1>
+      <input class="create" onKeyPress={this.createNew.bind(this)} />
+      <input class="filter" value={filter} onChange={this.filter.bind(this)} />
+      <ul>{todoList}</ul>
+      <a href="#" onClick={clearComplete}>Clear Complete</a>
+    </div>;
+   };
+ }
